754d034dab64a6e953d1f9ad7b097b641aafa0f7
Factum <art3que@gmail.com>
2024-06-05 19:55:36 +0200

    start page hero section animation

--- src/Components/AvatarMain.jsx
+++ src/Components/AvatarMain.jsx
@@ -1,6 +1,8 @@
 function AvatarMain() {
   return (
-	<img src="Avatars/Main/AvatarMainAnimation.gif" alt="Ahmed Lemssiah Web Designer" />
+	// <img src="Avatars/Main/AvatarMainAnimation.gif" alt="Ahmed Lemssiah Web Designer" />
+	<img src="/Avatars/Main/AvatarMainAnimation_2.gif" alt="Ahmed Lemssiah Web Designer" />
+
   )
 }
 
