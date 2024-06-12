faa958787bec9f86875c4f9b4a033cfacb20c846
Ahmed Lemssiah <art3que@gmail.com>
2024-06-12 13:30:34 +0200

    start Skill PAge

--- src/Components/StartHero.jsx
+++ src/Components/StartHero.jsx
@@ -1,21 +1,16 @@
-import { useEffect } from "react"
 
 import "../Styles/StartHero.scss"
 import AvatarMain from "./AvatarMain"
 import CallBtn from "./Shared/CallBtn"
 
-const animationCanvas = () => {
-  const canvas = document.querySelector("#start-hero-canvas")
-  const ctx = canvas.getContext("2d")
-  console.log(ctx)
-}
+
 
 
 function StartHero() {
   return (
     <section id="hero">
         <div className="wrapper">
-          <canvas id="start-hero-canvas" onLoad={animationCanvas}></canvas>
+          
           <div className="content">
             <h2>
               <span className="top">
